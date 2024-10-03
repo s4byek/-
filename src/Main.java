a67b7e75766e564f4385e8a69ba18b2aeb01b633
s4byek <borodulia.d@yandex.ru>
2024-10-04 02:53:57 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -22,14 +22,14 @@ public class Main {
                 "postgres",
                 "26112005"); // открытие соединения с базой данных
         System.out.println("Connected to the database");
-//        getAllPatients(connection);
-//        getAllTests(connection);
-//        getAllDoctors(connection);
+        getAllPatients(connection);
+        getAllTests(connection);
+        getAllDoctors(connection);
 //
 //        searchPatientByLastName(connection,"Зубенко",true);
 //        searchDoctorByID(connection, 8,true);
 //        searchTestById(connection, 1, true);
-
+//
 //        searcAllTestsForPatient(connection, "Шаурма");
 
 //        addPatient(connection, "Дмитрий", "Белинский", "м",   "89950191488");
@@ -41,56 +41,55 @@ public class Main {
 
     // Метод получения всех пациентов
     public static void getAllPatients(Connection connection) throws SQLException {
-        String query = "SELECT * FROM Пациент";
+        String query = "SELECT * FROM patients";
         try (Statement statement = connection.createStatement();
              ResultSet resultSet = statement.executeQuery(query)) {
             while (resultSet.next()) {
-                System.out.println("ID: " + resultSet.getInt("ID") +
-                        ", имя: " + resultSet.getString("Имя") + " " + resultSet.getString("Фамилия"));
+                System.out.println("id: " + resultSet.getInt("id") +
+                        ", first_name: " + resultSet.getString("first_name") + " " + resultSet.getString("last_name"));
             }
         }
     }
 
     public static void getAllTests(Connection connection) throws SQLException {
-        String query = "SELECT * FROM Анализы";
+        String query = "SELECT * FROM tests";
         try (Statement statement = connection.createStatement();
              ResultSet resultSet = statement.executeQuery(query)) {
             while (resultSet.next()) {
-                System.out.println("|" + resultSet.getInt("TestID") + "|" + resultSet.getString("TestName") + "|" + resultSet.getDouble("Price"));
+                System.out.println("|" + resultSet.getInt("test_id") + "|" + resultSet.getString("test_name") + "|"
+                        + resultSet.getDouble("price"));
             }
         }
     }
 
     public static void getAllDoctors(Connection connection) throws SQLException {
-        String query = "SELECT * FROM Врач";
+        String query = "SELECT * FROM doctors";
         try (Statement statement = connection.createStatement();
              ResultSet resultSet = statement.executeQuery(query)) {
             while (resultSet.next()) {
-                System.out.println("DoctorID: " + resultSet.getInt("DoctorID") +
-                        " |FirstName: " + resultSet.getString("FirstName") +
-                        " |LastName: " + resultSet.getString("LastName") +
-                        " |Specialization: " + resultSet.getString("Specialization") +
-                        " |ContactNumber: " + resultSet.getString("ContactNumber"));
-
+                System.out.println("doctor_id: " + resultSet.getInt("doctor_id") +
+                        " |first_name: " + resultSet.getString("first_name") +
+                        " |last_name: " + resultSet.getString("last_name") +
+                        " |specialization: " + resultSet.getString("specialization") +
+                        " |contact_number: " + resultSet.getString("contact_number"));
             }
         }
     }
 
-    //методы поисков
     public static int searchPatientByLastName(Connection connection, String lastName, boolean flag) throws SQLException {
-        String query = "SELECT * FROM \"Пациент\" WHERE \"Фамилия\" = ?";
+        String query = "SELECT * FROM patients WHERE last_name = ?";
         try (PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setString(1, lastName);
             try (ResultSet resultSet = statement.executeQuery()) {
                 while (resultSet.next()) {
                     if (flag) {
-                        System.out.println("ID: " + resultSet.getInt("ID") +
-                                ", Имя: " + resultSet.getString("Имя") +
-                                ", Фамилия: " + resultSet.getString("Фамилия") +
-                                ", Пол: " + resultSet.getString("Пол") +
-                                ", Контактный номер: " + resultSet.getString("Контактный номер"));
+                        System.out.println("id: " + resultSet.getInt("id") +
+                                ", first_name: " + resultSet.getString("first_name") +
+                                ", last_name: " + resultSet.getString("last_name") +
+                                ", gender: " + resultSet.getString("gender") +
+                                ", contact_number: " + resultSet.getString("contact_number"));
                     } else {
-                        return resultSet.getInt("ID");
+                        return resultSet.getInt("id");
                     }
                 }
             }
@@ -99,65 +98,62 @@ public class Main {
     }
 
     public static String searchDoctorByID(Connection connection, int doctorId, boolean flag) throws SQLException {
-        String query = "SELECT * FROM Врач WHERE \"DoctorID\" = ?";
+        String query = "SELECT * FROM doctors WHERE doctor_id = ?";
         try (PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setInt(1, doctorId);
             ResultSet resultSet = statement.executeQuery();
             if (resultSet.next()) {
                 if (flag) {
-                    System.out.println("DoctorID: " + resultSet.getInt("DoctorID") +
-                            " |FirstName: " + resultSet.getString("FirstName") +
-                            " |LastName: " + resultSet.getString("LastName") +
-                            " |Specialization: " + resultSet.getString("Specialization") +
-                            " |ContactNumber: " + resultSet.getString("ContactNumber"));
+                    System.out.println("doctor_id: " + resultSet.getInt("doctor_id") +
+                            " |first_name: " + resultSet.getString("first_name") +
+                            " |last_name: " + resultSet.getString("last_name") +
+                            " |specialization: " + resultSet.getString("specialization") +
+                            " |contact_number: " + resultSet.getString("contact_number"));
                 }
             } else {
                 System.out.println("Doctor with ID " + doctorId + " not found");
             }
-            return resultSet.getString("FirstName") + " " + resultSet.getString("LastName");
+            return resultSet.getString("first_name") + " " + resultSet.getString("last_name");
         }
     }
 
     public static String searchTestById(Connection connection, int testId, boolean flag) throws SQLException {
-        String query = "SELECT * FROM Анализы WHERE \"TestID\" = ?";
+        String query = "SELECT * FROM tests WHERE test_id = ?";
         try (PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setInt(1, testId);
             ResultSet resultSet = statement.executeQuery();
             if (resultSet.next()) {
                 if (flag) {
-                    System.out.println(resultSet.getInt("TestID") +
-                            "|" + resultSet.getString("TestName") +
-                            "|" + resultSet.getDouble("Price"));
+                    System.out.println(resultSet.getInt("test_id") +
+                            "|" + resultSet.getString("test_name") +
+                            "|" + resultSet.getDouble("price"));
                 }
             } else {
                 System.out.println("Test with ID " + testId + " not found");
-
             }
-            return resultSet.getString("TestName");
+            return resultSet.getString("test_name");
         }
     }
 
-
     public static void searcAllTestsForPatient(Connection connection, String lastName) throws SQLException {
-        int patinetid = searchPatientByLastName(connection, lastName, false);  //для удобства поиска по фамилии
-        String query = "SELECT * FROM \"Результаты анализов\" WHERE \"PatientID\"= " + patinetid;
+        int patinetid = searchPatientByLastName(connection, lastName, false);
+        String query = "SELECT * FROM test_results WHERE patient_id= " + patinetid;
         try (Statement statement = connection.createStatement();
              ResultSet resultSet = statement.executeQuery(query)) {
             while (resultSet.next()) {
-                String doc = searchDoctorByID(connection, resultSet.getInt("DoctorID"), false);
-                String test = searchTestById(connection, resultSet.getInt("TestID"), false);
-                System.out.println("ID: " + resultSet.getInt("ID") +
-                        ", Test: " + test +
-                        ", Doctor: " + doc +
-                        ", TestDate: " + resultSet.getDate("TestDate") +
-                        ", ResultValue: " + resultSet.getString("ResultValue"));
+                String doc = searchDoctorByID(connection, resultSet.getInt("doctor_id"), false);
+                String test = searchTestById(connection, resultSet.getInt("test_id"), false);
+                System.out.println("id: " + resultSet.getInt("id") +
+                        ", test: " + test +
+                        ", doctor: " + doc +
+                        ", test_date: " + resultSet.getDate("test_date") +
+                        ", result_value: " + resultSet.getString("result_value"));
             }
         }
     }
 
-
     public static void addPatient(Connection connection, String firstName, String lastName, String gender, String contactNumber) throws SQLException {
-        String query = "INSERT INTO \"Пациент\" (\"Имя\", \"Фамилия\", \"Пол\", \"Контактный номер\") VALUES (?, ?, ?, ?)";
+        String query = "INSERT INTO patients (first_name, last_name, gender, contact_number) VALUES (?, ?, ?, ?)";
         try (PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setString(1, firstName);
             statement.setString(2, lastName);
@@ -165,27 +161,28 @@ public class Main {
             BigDecimal contactNumberBigDecimal = new BigDecimal(contactNumber);
             statement.setBigDecimal(4, contactNumberBigDecimal);
             int rowsInserted = statement.executeUpdate();
-            if (rowsInserted > 0) {                                   //возвращает количетсво изменненных строк
-                System.out.println("Пациент успешно добавлен");
+            if (rowsInserted > 0) {
+                System.out.println("Patient successfully added");
             }
         }
     }
 
     public static void deletePatient(Connection connection, int patientId) throws SQLException {
-        String query = "DELETE FROM \"Пациент\" WHERE \"ID\" = ?";
+        String query = "DELETE FROM patients WHERE id = ?";
         try (PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setInt(1, patientId);
             int rowsDeleted = statement.executeUpdate();
             if (rowsDeleted > 0) {
-                System.out.println("Пациент успешно удален");
+                System.out.println("Patient successfully deleted");
             } else {
-                System.out.println("Пациент с указанным ID не найден");
+                System.out.println("Patient with the specified ID not found");
             }
         }
     }
 
-    public static void updatePatient(Connection connection, int patientId, String firstName, String lastName, String gender, String contactNumber) throws SQLException {
-        String query = "UPDATE \"Пациент\" SET \"Имя\" = ?, \"Фамилия\" = ?, \"Пол\" = ?, \"Контактный номер\" = ? WHERE \"ID\" = ?";
+    public static void updatePatient(Connection connection, int patientId, String firstName, String lastName, String gender, String contactNumber)
+            throws SQLException {
+        String query = "UPDATE patients SET first_name = ?, last_name = ?, gender = ?, contact_number = ? WHERE id = ?";
         try (PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setString(1, firstName);
             statement.setString(2, lastName);
@@ -195,26 +192,27 @@ public class Main {
             statement.setInt(5, patientId);
             int rowsUpdated = statement.executeUpdate();
             if (rowsUpdated > 0) {
-                System.out.println("Информация о пациенте успешно обновлена");
+                System.out.println("Patient information successfully updated");
             } else {
-                System.out.println("Пациент с указанным ID не найден");
+                System.out.println("Patient with the specified ID not found");
             }
         }
     }
+
     public static void patientDateTest(Connection connection) {
-        String query = "SELECT p.\"ID\", p.\"Имя\", p.\"Фамилия\", r.\"TestDate\", r.\"ResultValue\" " +
-                "FROM \"Пациент\" p " +
-                "JOIN \"Результаты анализов\" r ON p.\"ID\" = r.\"PatientID\"";
+        String query = "SELECT p.id, p.first_name, p.last_name, r.test_date, r.result_value " +
+                "FROM patient p " +
+                "JOIN test_results r ON p.id = r.patient_id";
         try (Statement statement = connection.createStatement();
              ResultSet resultSet = statement.executeQuery(query)) {
             while (resultSet.next()) {
-                String firstName = resultSet.getString("Имя");
-                String lastName = resultSet.getString("Фамилия");
-                Date testDate = resultSet.getDate("TestDate");
-                String resultValue = resultSet.getString("ResultValue");
-                System.out.println("Пациент: " + firstName + " " + lastName +
-                        ", Дата анализа: " + testDate +
-                        ", Результат: " + resultValue);
+                String firstName = resultSet.getString("first_name");
+                String lastName = resultSet.getString("last_name");
+                Date testDate = resultSet.getDate("test_date");
+                String resultValue = resultSet.getString("result_value");
+                System.out.println("Patient: " + firstName + " " + lastName +
+                        ", Test date: " + testDate +
+                        ", Result: " + resultValue);
             }
         } catch (SQLException e) {
             e.getMessage();
